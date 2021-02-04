e42b4614ef431b7c41f58086a318068be5fd4446
David <solorzanosarmientodavid@hotmail.com>
2021-02-04 15:26:02 -0500

    auto dibujo de malla

--- proyectos_desarrollo_web/Canvas/dibujo.js
+++ proyectos_desarrollo_web/Canvas/dibujo.js
@@ -1,49 +1,55 @@
-document.addEventListener('DOMContentLoaded', init);
+//alert("Sí funciona");
 
-function init(){
-    
-    //Canvas
-    const canvas=document.getElementById("dibujo");
-    const lienzo= canvas.getContext("2d");
-    const rect=canvas.getBoundingClientRect();
-    var x=0, y=0, dibujando=false;
-    const width= window.innerWidth;
-    const height= window.innerHeight;
-    canvas.width= width;
-    canvas.height= height;
+var d = document.getElementById("dibujo"); // Obtener valor desde el html
+var lienzo = d.getContext("2d"); // Parte del lienzo
+var l = 0;
+var l2 = 0;
+var lineas=50;
+var yi,xf;
 
+//dibujarLinea("red", 0,0,10,300);
+console.log(lienzo);
 
-    canvas.addEventListener('mousedown', (e) =>{
-        x= e.clientX - rect.left; 
-        y= e.clientY - rect.top;
-        dibujando= true;
-    });
-    
-    canvas.addEventListener('mousemove', (e) =>{
-        if(dibujando===true){
-            dibujar(x, y, e.clientX - rect.left , e.clientY - rect.top);
-            x= e.clientX - rect.left;
-            y= e.clientY - rect.top;
-        }    
-    });
+while(l<lineas){
+	yi = 10 * l;
+	xf = 10 * (l+1);
+	dibujarLinea(obtenerColorAleatorio(), 0, yi, xf,500);
+	dibujarLinea(obtenerColorAleatorio(), 500, yi, xf,0);
+	//dibujarLinea(color3, 500, yi, xf,0);
+	console.log("Linea es: "+l);
+	l++;
+}
+// Borde inferior izquierdoobtenerColorAleatorio()
+dibujarLinea(obtenerColorAleatorio(), 1,1,1,499);
+dibujarLinea(obtenerColorAleatorio(), 1,499,499,499);
 
-    canvas.addEventListener('mouseup', (e) =>{
-        if(dibujando===true){
-            dibujar(x, y, e.clientX - rect.left , e.clientY - rect.top);
-            x= 0; 
-            y= 0;
-            dibujando= false;
-        }
-    });
+// Borde superior derecho
+dibujarLinea(obtenerColorAleatorio(), 499,499,499,1);
+dibujarLinea(obtenerColorAleatorio(), 499,1,1,1);
 
-    function dibujar( x1, y1, x2, y2){
-        lienzo.beginPath();
-        lienzo.lineWidth =2;
-        lienzo.moveTo(x1, y1);
-        lienzo.lineTo(x2, y2);
-        lienzo.stroke();
-        lienzo.closePath();
-    }
+function dibujarLinea (color, xinicial, yinicial, xfinal, yfinal){
+	lienzo.beginPath(); // Iniciar trazado
+	lienzo.strokeStyle = color; // Propiedad => Color de la línea
+	lienzo.moveTo(xinicial,yinicial); // Inicio del punto de trazado
+	lienzo.lineTo(xfinal,yfinal); // Final del punto de trazado
+	lienzo.stroke(); // Dibuja la línea
+	lienzo.closePath(); // Cerrar el trazo, levantar el lápiz
 }
 
+function numeroAletario(inferior, superior){
+	numPosibilidades = superior-inferior;
+	numero = Math.floor(Math.random()*numPosibilidades);
+	return parseInt(inferior) + numero;
+}
 
+function obtenerColorAleatorio(){
+	hexadecimalArray = new Array("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F");
+	colorAleatorio="#"; // Inicia el primer valor del color hexadecimal
+
+	// Generar un color aletario para cada línea
+	for (i=0;i<6;i++){
+		numero = numeroAletario(0, hexadecimalArray.length); // define un número aleatorio entre 0 el tamaño del array de los colores hexadecimales
+		colorAleatorio += hexadecimalArray[numero]; // Concatena el string del color
+	}
+	return colorAleatorio;
+}
