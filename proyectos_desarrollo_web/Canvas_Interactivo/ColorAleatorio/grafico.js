838a92cad58ade86170eed49863205f9f1c5381c
David <solorzanosarmientodavid@hotmail.com>
2021-02-08 10:09:24 -0500

    ahora si el cambio del tablero paint y los graficos con formularios

--- proyectos_desarrollo_web/Canvas_Interactivo/ColorAleatorio/grafico.js
+++ proyectos_desarrollo_web/Canvas_Interactivo/ColorAleatorio/grafico.js
@@ -1,60 +1,78 @@
 //alert("Sí funciona");
 
-var canvas = document.getElementById("dibujo"); // Obtener valor desde el html
-var lienzo = canvas.getContext("2d"); // Parte del lienzo
-const width= window.innerWidth; //obtemos el ancho de la ventan
-const height= window.innerHeight; //obtemos el alto de la ventan
-canvas.width= width; //Asignamos ancho a la variable canvas
-canvas.height= height; //Asignamos alto a la variable canvas
-var l = 0;
-var l2 = 0;
-var lineas=50;
-var yi,xf;
+const canvas = document.getElementById("dibujo"); // Obtener valor desde el html
+const lienzo = canvas.getContext("2d"); // Parte del lienzo
 
-console.log(lienzo);
+const caja = document.getElementById("tamaño");
+const graf = document.getElementById("lineas");
+const boton = document.getElementById("accion");
+const rgb = document.getElementById("color");
 
-while(l<lineas){
-	yi = 10 * l;
-	xf = 10 * (l+1);
-	dibujarLinea(obtenerColorAleatorio(), 0, yi, xf,500);
-	dibujarLinea(obtenerColorAleatorio(), 500, yi, xf,0);
-	console.log("Linea es: "+l);
-	l++;
+boton.addEventListener('click', DibujoClick);
+
+function DibujoClick(){
+
+	var dimension = parseInt(caja.value);
+	var lineas= parseInt(graf.value);
+	var color= rgb.value;
+	canvas.width = dimension; 
+	canvas.height = dimension;
+	console.log(dimension);
+	console.log(color);
+	var l = 0;
+	var esp = dimension / lineas;
+	var yi,xf;
+
+	if(color!= "#000000"){
+		for( l=0; l< lineas; l++){
+			yi = esp * l;
+			xf = esp * (l+1);
+			dibujarLinea(color, 0, yi, xf,dimension);
+			dibujarLinea(color, dimension, yi, xf,0);
+		}
+		// Borde inferior izquierdoobtenerColorAleatorio()
+		dibujarLinea(color, 1,1,1,dimension-1);
+		dibujarLinea(color, 1,dimension-1,dimension-1,dimension-1);
+		
+		// Borde superior derecho
+		dibujarLinea(color, dimension-1,dimension-1,dimension-1,1);
+		dibujarLinea(color, dimension-1,1,1,1);
+
+	} else{
+		for( l=0; l< lineas; l++){
+			yi = esp * l;
+			xf = esp * (l+1);
+			dibujarLinea(colorRGB(), 0, yi, xf,dimension);
+			dibujarLinea(colorRGB(), dimension, yi, xf,0);
+		}
+		// Borde inferior izquierdoobtenerColorAleatorio()
+		dibujarLinea(colorRGB(), 1,1,1,dimension-1);
+		dibujarLinea(colorRGB(), 1,dimension-1,dimension-1,dimension-1);
+		
+		// Borde superior derecho
+		dibujarLinea(colorRGB(), dimension-1,dimension-1,dimension-1,1);
+		dibujarLinea(colorRGB(), dimension-1,1,1,1);
+	}
 }
-// Borde inferior izquierdoobtenerColorAleatorio()
-dibujarLinea(obtenerColorAleatorio(), 1,1,1,499);
-dibujarLinea(obtenerColorAleatorio(), 1,499,499,499);
 
-// Borde superior derecho
-dibujarLinea(obtenerColorAleatorio(), 499,499,499,1);
-dibujarLinea(obtenerColorAleatorio(), 499,1,1,1);
 
 // funcion #1 en invocarse
-function dibujarLinea (color, xinicial, yinicial, xfinal, yfinal){
+function dibujarLinea (col, x1, y1, x2, y2){
 	lienzo.beginPath(); // Iniciar trazado
-	lienzo.strokeStyle = color; // Propiedad => Color de la línea
-	lienzo.moveTo(xinicial,yinicial); // Inicio del punto de trazado
-	lienzo.lineTo(xfinal,yfinal); // Final del punto de trazado
+	lienzo.strokeStyle = col; // Propiedad => Color de la línea
+	lienzo.moveTo(x1,y1); // Inicio del punto de trazado
+	lienzo.lineTo(x2,y2); // Final del punto de trazado
 	lienzo.stroke(); // Dibuja la línea
 	lienzo.closePath(); // Cerrar el trazo, levantar el lápiz
 }
 
 // funcion #2 en invocarse
-function obtenerColorAleatorio(){
-	hexadecimalArray = new Array("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F");
-	colorAleatorio="#"; // Inicia el primer valor del color hexadecimal
-
-	// Generar un color aletario para cada línea
-	for (i=0;i<6;i++){
-		numero = numeroAletario(0, hexadecimalArray.length); // define un número aleatorio entre 0 el tamaño del array de los colores hexadecimales
-		colorAleatorio += hexadecimalArray[numero]; // Concatena el string del color
-	}
-	return colorAleatorio;
+function colorRGB(){
+	var coolor = "("+generarNumero(255)+"," + generarNumero(255) + "," + generarNumero(255) +")";
+	return "rgb" + coolor;
 }
 
 // funcion #3 en invocarse
-function numeroAletario(inferior, superior){
-	numPosibilidades = superior-inferior;
-	numero = Math.floor(Math.random()*numPosibilidades);
-	return parseInt(inferior) + numero;
+function generarNumero(numero){
+	return (Math.random()*numero).toFixed(0);
 }
