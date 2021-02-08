2e1cf1d59e30e8dc9c2cf06a20f663749d974516
David <solorzanosarmientodavid@hotmail.com>
2021-02-08 11:47:13 -0500

    nuevo ejemplo canvas

---
+++ proyectos_desarrollo_web/Canvas_flechas/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', init);
+
+function init(){
+    const canvas= document.getElementById("tablero");
+    const lienzo = canvas.getContext("2d");
+    const rect= canvas.getBoundingClientRect();
+
+    
+}
